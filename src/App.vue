a1be344fa81cd9ccb1726a0874e8bbc493c693b5
Matija Petek <mpetek@tvz.hr>
2020-11-05 21:48:10 +0100

    Initialize git

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li><a href="default.asp">Home</a></li>
+      <li><a href="news.asp">News</a></li>
+      <li><a href="contact.asp">Contact</a></li>
+      <li><a href="about.asp">About</a></li>
+    </ul>
+    <div class="main">
+    
+    </div>
+    <AddActivities msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddActivities from './components/AddActivities.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AddActivities
+  },
+  data() {
+            return {
+                
+            }
+        }
 }
 </script>
 
@@ -23,6 +36,34 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:60px auto;
+  flex: 1;
+  max-width: 800px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  border-radius: 10px;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
 }
 </style>
---
+++ src/components/AddActivities.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="main">
+      
+   <form>
+       <h2> Activity Suggester</h2>
+       <p id="description">Tell us how do You feel and we will suggest an activty!</p>
+       <b-field>
+            <b-select placeholder="Select">
+                <option value='bored' name="bored">Bored</option>
+                <option value="chilled" name="chilled">Chilled</option>
+                <option value="energy" name="energy">Full of energy</option>
+                <option value="lonely" name="lonely">Lonely</option>
+            </b-select>
+        </b-field>
+       <button type="submit">Submit</button>
+   </form>
+   <h3> Chosen activity:</h3>
+   <div class="display">
+
+
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+form{
+    display: block;
+    all:unset;
+    margin-top: 20px;
+    padding-top: 20px;
+}
+
+
+input{
+    all:unset;
+    border: 1px solid lightgray;
+    padding:12px;
+    text-align: left;
+    background-color: lightgray;
+    border-radius:10px;
+}
+
+button{
+    all:unset;
+    background:black;
+    color:white;
+    padding:12px;
+    margin-left: 15px;
+    text-align: left;
+    border-radius: 10px;
+}
+
+.main{
+    background: rgb(113,107,251);
+    background: linear-gradient(180deg, rgba(113,107,251,1) 0%, rgba(153,131,247,1) 54%, rgba(255,241,245,1) 100%);
+    height:450px;
+    border-radius: 10px;
+
+}
+
+#input{
+    margin-top:30px;
+}
+
+h2{
+    color:white;
+    font-size: 40px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+h3{
+    margin-top: 5px;
+    color:white;
+    font-size: 40px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+#description{
+    color:white;
+}
+
+
+.display{
+    width:50%;
+    border-radius: 10px;
+    height: 45px;
+    background-color: white;
+    margin:75px auto;
+    color:black;
+}
+
+
+
+</style>
